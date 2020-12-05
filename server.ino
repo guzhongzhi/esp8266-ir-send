148d0a21978f8ed92ef31f5530a2808ff15d4edc
guzhongzhi <guzhongzhi@qq.com>
2020-12-05 13:30:19 +0800

    code for this

--- server.ino
+++ server.ino
@@ -42,6 +42,7 @@ void handleRoot()
               "<html>"
               "<head><title>ESP8266 Demo</title></head>"
               "<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">"
+              "<meta name='viewport' content='width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'/>"
               "<body>"
               "<h1>控制中心</h1>"
               "<div id='loading'>Loading</div>"
