700b75732cbff3aae5165d3996485da4aa6be23c
guzhongzhi <guzhongzhi@qq.com>
2020-12-05 23:14:41 +0800

    code for this

--- server.ino
+++ server.ino
@@ -44,12 +44,12 @@ void handleRoot()
               "<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">"
               "<meta name='viewport' content='width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'/>"
               "<body>"
-              "<div class='title'><h1>控制中心</h1></div>"
+              "<div class='title'><h1>控制中心<span id='loading'>Loading</span></h1></div>"
               "<script src='https://libs.baidu.com/jquery/2.0.0/jquery.min.js'></script>"
               "<script src='https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min.js'></script>"
               "<script src='http://esp8266.gulusoft.com/config.js'></script>"
               "<link rel='stylesheet' type='text/css' href='http://esp8266.gulusoft.com/main.css'>"
-              "<div id='content'><div id='loading'>Loading</div></div>"
+              "<div id='content'></div>"
               "</body>"
               "</html>");
 }
